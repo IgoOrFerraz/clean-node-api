3aa1fb28d63d3183abff2550bf984628ac1ffec3
IgoOr Ferraz <igoorferraz23@gmail.com>
2023-08-21 21:58:22 -0300

    refactor: adjusts cors test

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -3,8 +3,8 @@ import app from '../config/app'
 
 describe('CORS Middleware', () => {
   test('Should enable CORS', async () => {
-    app.post('/test_cors', (req, res) => {
-      res.send(req.body)
+    app.get('/test_cors', (req, res) => {
+      res.send()
     })
     await request(app)
       .get('/test_cors')
